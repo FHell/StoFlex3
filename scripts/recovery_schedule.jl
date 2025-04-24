b621d3adac1030461e46934dc925b81782bac301
frank <hellmann@pik-potsdam.de>
2025-04-24 17:33:42 +0200

    Additional recovery schedules plotted

--- scripts/recovery_schedule.jl
+++ scripts/recovery_schedule.jl
@@ -22,11 +22,11 @@ data_es = load_industry_park1()
 # experiment = "no_solar"
 # experiment = "large_heat"
 
-if ! @isdefined experiment
+if !@isdefined experiment
     experiment = "defaults"
 end
 
-if ! @isdefined dont_display
+if !@isdefined dont_display
     dont_display = false
 end
 
@@ -41,15 +41,15 @@ mkpath(figure_dir)
 
 ##
 
-df_exp = filter_kwd(df; first_dir = experiment)
+df_exp = filter_kwd(df; first_dir=experiment)
 
 
 CB = get_CB(df)
 
 flex_interval = 12
 n_samples = 12
-F=500
-df = filter_kwd(df_exp; flex_interval, name = "OFIOR", n_samples, F)
+F = 500
+df = filter_kwd(df_exp; flex_interval, name="OFIOR", n_samples, F)
 
 
 ##
@@ -67,12 +67,12 @@ optimize!(sp)
 ##
 
 function app2(sym)
-    Symbol(String(sym)*"2")
+    Symbol(String(sym) * "2")
 end
 
 
-function plot_recovery_agg(sp; scens, recovery_time, s = 1)
-    update_theme!(fontsize =30)
+function plot_recovery_agg(sp; scens, recovery_time, s=1)
+    update_theme!(fontsize=30)
 
     df_1 = DataFrame()
     df_2 = DataFrame()
@@ -82,7 +82,7 @@ function plot_recovery_agg(sp; scens, recovery_time, s = 1)
     F_xi = scens.F_xi[s]
     println("time=$(t_xi), sign=$s_xi, size=$F_xi")
 
-    recovery_window = (t_xi):(t_xi + recovery_time)
+    recovery_window = (t_xi):(t_xi+recovery_time)
 
     df_1.t = recovery_window
     df_2.t = recovery_window
@@ -91,9 +91,9 @@ function plot_recovery_agg(sp; scens, recovery_time, s = 1)
     var_out = [:gco, :sto_to_bus, nothing, nothing]
 
     recs = ["Grid Connection\nkW",
-    "Battery Charging\nkW",
-    "Battery SOC\nkWh",
-    "Heatpump\nkW"]
+        "Battery Charging\nkW",
+        "Battery SOC\nkWh",
+        "Heatpump\nkW"]
 
     for (vi, vo, vr) in zip(var_in, var_out, recs)
         df_1[!, vr] = value.(sp[vi][recovery_window])
@@ -104,21 +104,21 @@ function plot_recovery_agg(sp; scens, recovery_time, s = 1)
     end
 
     fig = Figure(;
-    figure_padding = (5, 5, 10, 10),
-    size = (800, 300 * length(recs)),)
+        figure_padding=(5, 5, 10, 10),
+        size=(800, 300 * length(recs)),)
 
     for (i, rec) in enumerate(recs)
         ax = Axis(fig[i, 1];
-        ylabel = rec)
+            ylabel=rec)
 
         lines!(ax, df_1.t,
             df_1[!, rec],
-            label = "1st Stage", color = :black, linewidth=5)
+            label="1st Stage", color=:black, linewidth=5)
         lines!(ax, df_2.t,
             df_2[!, rec],
-            label = "2nd Stage", color = :red, linestyle=:dash, linewidth=5)
+            label="2nd Stage", color=:red, linestyle=:dash, linewidth=5)
     end
-    fig.content 
+    fig.content
 
     reset_limits!.(fig.content)
     yspace = maximum(tight_yticklabel_spacing!, fig.content)
@@ -134,10 +134,10 @@ end
 # Find scenarios that are excessively expensive:
 
 ##
-mkpath(joinpath(figure_dir,"publication"))
+mkpath(joinpath(figure_dir, "publication"))
 
 ##
-for s in 10:14
+for s in [10:14..., 100:140...]
 
     fig = plot_recovery_agg(sp; scens, recovery_time, s)
 
